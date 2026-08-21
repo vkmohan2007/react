15c2bb1f6a2d9efea6c58bae72add88026221adb
vkmohan2007 <vkmohan2007@gmail.com>
2026-08-21 19:43:55 +0530

    VPS setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <h1>Header</h1>
+      <h1>Header <span style="color:green">[ Edited on 21:08:2026 - 19:29 ]</span> </h1>
     </>
   )
 }
